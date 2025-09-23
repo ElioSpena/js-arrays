72f2acc207537bc5ba07f1e0369c976484cd3873
ElioSpena <elio.spena.94@gmail.com>
2025-09-23 17:54:45 +0200

    feat: snack completi

--- 03-snack-bonus/script.js
+++ 03-snack-bonus/script.js
@@ -16,20 +16,59 @@ console.log(randomNum);
 // 2. Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
 
 const intNum = [10, 25, 16, 44, 32, 64];
-let somma = 0;
+let sum = 0;
 
 for (let i = 0; i < intNum.length; i++) {
 
     if (i % 2 !== 0) {
-        somma += intNum[i];
+        sum += intNum[i];
     }
 
 }
 
-console.log(somma);
+console.log(sum);
 
 // 3. Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
+const userNumber = prompt("Scrivi un numero di 4 cifre:", 5434);
+
+const userNumberAsArray = userNumber.split("");
+let userNumberSum = 0;
+
+for (let i = 0; i < userNumberAsArray.length; i++) {
+    userNumberSum += parseInt(userNumberAsArray[i]);
+
+}
+
+console.log(userNumberSum);
+
 // 4. Calcola la somma e la media dei primi 10 numeri.
 
+const numbers = [];
+let meanNum = 0;
+
+for (let i = 1; i <= 10; i++) {
+
+    numbers.push(i);
+    meanNum = (numbers.length * i) / 10;
+
+}
+console.log(meanNum);
+
+
 // 5. Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome
+
+const names = ["Jeff", "Tom", "Lily", "Hannah", "Robert"];
+const surnames = ["Grossman", "Peters", "Johnson", "Potter", "Draper"];
+
+
+let randomInvited = [];
+
+for (let i = 0; i < 10; i++) {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    const randomSurname = surnames[Math.floor(Math.random() * surnames.length)];
+    const fullName = `${randomName} ${randomSurname}`;
+    randomInvited.push(fullName);
+}
+
+console.log(randomInvited);
